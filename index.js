f5b46ec047ddf3e3f2976fad6b107a58e6bf6679
NizamChy <nizam.chy.cs@gmail.com>
2023-10-25 22:55:50 +0600

    deploy problem fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// backend database index.js file
+// backend database index.js filemidleware
 
 const express = require("express");
 const cors = require("cors");
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // ------------------
 
-const uri = `mongodb+srv://brandshop10:${process.env.DB_PASS}@cluster0.9s9fb7y.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9s9fb7y.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,7 +28,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // creating database for cart item collection
     const cartItemCollection = client.db("cartItemDB").collection("mycart");
@@ -140,7 +140,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
